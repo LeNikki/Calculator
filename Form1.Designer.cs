89945d7256750d20a600d606ff90aa0391854e30
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2021-06-23 13:54:34 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.btnPercent = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBackspace = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPercent
+            // 
+            resources.ApplyResources(this.btnPercent, "btnPercent");
+            this.btnPercent.BackColor = System.Drawing.Color.Thistle;
+            this.btnPercent.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnPercent.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnPercent.Name = "btnPercent";
+            this.btnPercent.UseVisualStyleBackColor = false;
+            this.btnPercent.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // btnZero
+            // 
+            resources.ApplyResources(this.btnZero, "btnZero");
+            this.btnZero.BackColor = System.Drawing.Color.Thistle;
+            this.btnZero.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnZero.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnZero.Name = "btnZero";
+            this.btnZero.UseVisualStyleBackColor = false;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnDot
+            // 
+            resources.ApplyResources(this.btnDot, "btnDot");
+            this.btnDot.BackColor = System.Drawing.Color.Thistle;
+            this.btnDot.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnDot.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDot.Name = "btnDot";
+            this.btnDot.UseVisualStyleBackColor = false;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
+            // 
+            // btnOne
+            // 
+            resources.ApplyResources(this.btnOne, "btnOne");
+            this.btnOne.BackColor = System.Drawing.Color.Thistle;
+            this.btnOne.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnOne.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnOne.Name = "btnOne";
+            this.btnOne.UseVisualStyleBackColor = false;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            resources.ApplyResources(this.btnTwo, "btnTwo");
+            this.btnTwo.BackColor = System.Drawing.Color.Thistle;
+            this.btnTwo.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnTwo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.UseVisualStyleBackColor = false;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            resources.ApplyResources(this.btnThree, "btnThree");
+            this.btnThree.BackColor = System.Drawing.Color.Thistle;
+            this.btnThree.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnThree.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnThree.Name = "btnThree";
+            this.btnThree.UseVisualStyleBackColor = false;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnFour
+            // 
+            resources.ApplyResources(this.btnFour, "btnFour");
+            this.btnFour.BackColor = System.Drawing.Color.Thistle;
+            this.btnFour.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnFour.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFour.Name = "btnFour";
+            this.btnFour.UseVisualStyleBackColor = false;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnFive
+            // 
+            resources.ApplyResources(this.btnFive, "btnFive");
+            this.btnFive.BackColor = System.Drawing.Color.Thistle;
+            this.btnFive.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnFive.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFive.Name = "btnFive";
+            this.btnFive.UseVisualStyleBackColor = false;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnSix
+            // 
+            resources.ApplyResources(this.btnSix, "btnSix");
+            this.btnSix.BackColor = System.Drawing.Color.Thistle;
+            this.btnSix.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnSix.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSix.Name = "btnSix";
+            this.btnSix.UseVisualStyleBackColor = false;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnEquals
+            // 
+            resources.ApplyResources(this.btnEquals, "btnEquals");
+            this.btnEquals.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnEquals.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnEquals.ForeColor = System.Drawing.Color.White;
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.UseVisualStyleBackColor = false;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // btnSeven
+            // 
+            resources.ApplyResources(this.btnSeven, "btnSeven");
+            this.btnSeven.BackColor = System.Drawing.Color.Thistle;
+            this.btnSeven.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnSeven.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.UseVisualStyleBackColor = false;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnEight
+            // 
+            resources.ApplyResources(this.btnEight, "btnEight");
+            this.btnEight.BackColor = System.Drawing.Color.Thistle;
+            this.btnEight.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnEight.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEight.Name = "btnEight";
+            this.btnEight.UseVisualStyleBackColor = false;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnNine
+            // 
+            resources.ApplyResources(this.btnNine, "btnNine");
+            this.btnNine.BackColor = System.Drawing.Color.Thistle;
+            this.btnNine.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnNine.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNine.Name = "btnNine";
+            this.btnNine.UseVisualStyleBackColor = false;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnAdd
+            // 
+            resources.ApplyResources(this.btnAdd, "btnAdd");
+            this.btnAdd.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnAdd.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // btnMultiply
+            // 
+            resources.ApplyResources(this.btnMultiply, "btnMultiply");
+            this.btnMultiply.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnMultiply.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnMultiply.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            this.btnMultiply.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnBackspace
+            // 
+            resources.ApplyResources(this.btnBackspace, "btnBackspace");
+            this.btnBackspace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnBackspace.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnBackspace.ForeColor = System.Drawing.Color.Black;
+            this.btnBackspace.Name = "btnBackspace";
+            this.btnBackspace.UseVisualStyleBackColor = false;
+            this.btnBackspace.Click += new System.EventHandler(this.btnBackspace_Click);
+            // 
+            // btnDivide
+            // 
+            resources.ApplyResources(this.btnDivide, "btnDivide");
+            this.btnDivide.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnDivide.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnDivide.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.Color.Plum;
+            resources.ApplyResources(this.Output, "Output");
+            this.Output.Name = "Output";
+            this.Output.UseWaitCursor = true;
+            this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
+            // 
+            // btnSubtract
+            // 
+            resources.ApplyResources(this.btnSubtract, "btnSubtract");
+            this.btnSubtract.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnSubtract.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnSubtract.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.UseVisualStyleBackColor = false;
+            this.btnSubtract.Click += new System.EventHandler(this.Operator_Click);
+            // 
+            // Calculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnBackspace);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnPercent);
+            this.Name = "Calculator";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPercent;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBackspace;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button btnSubtract;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        double value = 0;  
+        string operand = "";
+        bool process = false;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Will set function for each number button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Number (object sender, EventArgs e)
+        {
+            if  ((Output.Text == "0") || (process == true))
+            {  Output.Clear();  }       //will clear the screen
+            process = false;            //to keep on screen next input after operation
+            Button bt = (Button)sender;
+            Output.Text = Output.Text + bt.Text; //shows what is on the text box 
+                                                 //+ new button to be clicked
+        }
+        
+        /// <summary>
+        /// This will determine which operations are pressed.
+        /// Added to events of each operation buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            operand = bt.Text; //will hold the operation clicked                               
+            process = true;    //if an operator is toggled
+            value = Double.Parse(Output.Text); //will convert what's on text box to double to perform it arithmetically
+        }
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Result text box
+        private void Output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnEquals_Click(object sender, EventArgs e) //if equals is toggled
+        {
+            process = false; // sets back to false to accept new operations
+            switch (operand)
+            {
+                case "+":                 
+                    Output.Text = ($"{ value + Double.Parse(Output.Text) }");
+                    break;
+                case "-":
+                    Output.Text = ($"{value - Double.Parse(Output.Text)}");
+                    break;
+                case "*":
+                    Output.Text = ($"{value * Double.Parse(Output.Text)}");
+                    break;
+                case "/":
+                    Output.Text = ($"{value / Double.Parse(Output.Text)}");
+                    break;
+                case "%":
+                    Output.Text = ($"{(value/100)*Double.Parse(Output.Text)}");
+                    break;
+                default: break;
+            }//switch 
+            
+        }
+
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+           Number(sender, e);
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            Number(sender, e);
+        }
+
+        //CLEAR SCREEN
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Output.Clear();
+            Output.Text = "0";
+        }
+
+        //BACKSPACE
+        private void btnBackspace_Click(object sender, EventArgs e)
+        {
+            if ((Output.Text.Length != 0))
+            { Output.Text = Output.Text.Substring(0, Output.Text.Length - 1); }
+            else
+            {
+                Output.Text = "0";
+            }
+        }
+
+    }
+}
